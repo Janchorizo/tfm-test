c49d4b3757eacc7010f21401545dcad60d11ab6d
Janchorizo <jancho@usal.es>
2019-05-01 17:53:43 +0200

    Add basic React project.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 window.onload = function(){
     console.log('hola')
     ReactDOM.render(
-      React.createElement('div', null, 'hello'),
+      e(App, null, null),
       document.getElementById('root')
     );
+}
+
+const e = React.createElement; 
+
+class App extends React.Component {
+  render() {
+    const main_container = e('div', {className: 'container'}, 'hi');
+    return main_container;
+  }
+}
+
+class TopMenu extends React.Component {
+  render() {
+    //return <div>Hello {this.props.toWhat}</div>;
+  }
 }
